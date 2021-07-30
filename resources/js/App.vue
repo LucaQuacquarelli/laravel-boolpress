703b3422c5634cf951b3f515f11a4f7c91a52b33
LucaQuacquarelli <lucaq82@libero.it>
2021-07-30 14:06:15 +0200

    Add: chiamata Api e passaggio data in App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,62 @@
 <template>
-    <BuildingSite/>
+    <div>    
+        <Header/>
+        <main class="container">
+
+        </main>
+        <Footer/>
+    </div>
 </template>
 
 <script>
-import BuildingSite from './components/BuildingSite.vue'
+import Header from './components/partials/Header.vue'
+import Footer from './components/partials/Footer.vue'
 export default {
     name : 'App',
     components: {
-        BuildingSite
+        Header,
+        Footer
     },
+    data: function() {
+        return {
+            posts: [],
+            current_page: 1,
+            last_page: 1,
+        }
+    },
+    methods: {
+        getPost: function(page = 1) {
+            axios
+                .get(`http://127.0.0.1:8000/api/posts?page=${page}`)
+                .then(
+                    res => {
+                        this.posts = res.data.data;
+                        this.current_page = res.data.current_page;
+                        this.last_page = res.data.last_page;
+                        this.posts.forEach(
+                            element => {
+                                element.excerpt = this.excerpt(element.content, 150);
+                            }
+                        );
+                    }
+                )
+                .catch(
+                    err => {
+                    console.log(err);
+                }
+            );
+        },
+        excerpt: function(string, charsNumber = 100) {
+            if(string.length > charsNumber) {
+                return string.substr(0, charsNumber) + '...';
+            } else {
+                return string;
+            }
+        },
+    },
+    created: function() {
+        this.getPost()
+    }
 
 }
 </script>
---
+++ resources/js/components/partials/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="bg-primary text-center">
+        <div class="container">
+            &copy; Wordpress 2021
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
